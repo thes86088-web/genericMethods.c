1bded3b399e9b3bdafe1582108fe74ae0ac394d0
durvesh <thes86088@gmail.com>
2026-02-03 21:36:22 +0530

    define array of required functions

--- methods.c
+++ methods.c
@@ -7,7 +7,7 @@ void *add( void *a, void *b ){
 
 size_t size = sizeof(a)>sizeof(b) ? sizeof(a) : sizeof(b) ;
 
-
+void*( *func[4] )(void *, void *, void *) = { add_int, add_doub, add_short, add_byte };
 
 void *funcCalled( void *a, void *b, size_t size, void*( *func[4] )(void *, void *, void *) )
 {
@@ -16,7 +16,7 @@ void *funcCalled( void *a, void *b, size_t size, void*( *func[4] )(void *, void 
   if( size == sizeof(int) )  type=0 ;
   else if( size == sizeof(double) ) tyre=1 ;
   else if( size == sizeof(short) ) type=2 ;
-  else if( size == sizeof(byte) ) type = 3;
+  else type = 3; //consider byte if other cases fail
   
    func[type]( a, b, result );
   
