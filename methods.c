a5da59f3873af04944725adedb0e8873c8d6b189
durvesh <thes86088@gmail.com>
2026-02-03 20:55:08 +0530

    *(func[0](a, b))  --> *(func[0](&a, &b))

--- methods.c
+++ methods.c
@@ -24,8 +24,8 @@ int *(*func[4])(int *, int *) = {add, sub, mul, divi} ;
 int main()
 {
   int a = 10; int b=20;
-  printf(" %d + %d = %d \n", a, b, *(func[0](a,b)) );
-  printf(" %d - %d = %d \n", a, b, *(func[1](a,b)) );
-  printf(" %d * %d = %d \n", a, b, *(func[2](a,b)) );
-  printf(" %d / %d = %d \n", a, b, *(func[3](a,b)) );
+  printf(" %d + %d = %d \n", a, b, *(func[0](&a, &b)) );
+  printf(" %d - %d = %d \n", a, b, *(func[1](&a, &b)) );
+  printf(" %d * %d = %d \n", a, b, *(func[2](&a, &b)) );
+  printf(" %d / %d = %d \n", a, b, *(func[3](&a, &b)) );
 }
