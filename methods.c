67e5c463caa19d8b85fa551651e04411a03ad138
durvesh <thes86088@gmail.com>
2026-02-03 21:34:32 +0530

    add "type" local variable

--- methods.c
+++ methods.c
@@ -12,18 +12,13 @@ size_t size = sizeof(a)>sizeof(b) ? sizeof(a) : sizeof(b) ;
 void *funcCalled( void *a, void *b, size_t size, void*( *func[4] )(void *, void *, void *) )
 {
   void *result = malloc(size);
-
-  if( size == sizeof(int) )
-    func[0]( a, b, result );
+  int type = 0;
+  if( size == sizeof(int) )  type=0 ;
+  else if( size == sizeof(double) ) tyre=1 ;
+  else if( size == sizeof(short) ) type=2 ;
+  else if( size == sizeof(byte) ) type = 3;
   
-  else if( size == sizeof(double) )
-    func[1]( a, b, result );
-  
-  else if( size == sizeof(short) )
-    func[2]( a, b, result );
-
-  else if( size == sizeof(byte) )
-    func[3]( a, b, result );
+   func[type]( a, b, result );
   
   return result ;
 }
