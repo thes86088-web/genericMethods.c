9399d6e508a0d17c95e39e3ed2a8c0b85dcddd60
durvesh <thes86088@gmail.com>
2026-02-03 21:59:38 +0530

    change byte --> long long

--- methods.c
+++ methods.c
@@ -1,3 +1,5 @@
+#include <stddef.h>'
+
 void *add_int( void *a, void *b, void *result ){ 
   *((int *)result) = *((int *)a) + *((int *)b) ; 
 }
@@ -10,22 +12,22 @@ void *add_short( void *a, void *b, void *result ){
   *((short *)result) = *((short *)a) + *((short *)b) ; 
 }
 
-void *add_byte( void *a, void *b, void *result ){ 
-  *((byte *)result) = *((byte *)a) + *((byte *)b) ; 
+void *add_ll( void *a, void *b, void *result ){ 
+  *((long long *)result) = *((long long *)a) + *((long long *)b) ; 
 }
 
-size_t size = sizeof(a)>sizeof(b) ? sizeof(a) : sizeof(b) ;
+//size_t size = sizeof(a)>sizeof(b) ? sizeof(a) : sizeof(b) ;
 
-void*( *func[4] )(void *, void *, void *) = { add_int, add_doub, add_short, add_byte };
+void*( *func[4] )(void *, void *, void *) = { add_int, add_doub, add_short, add_ll };
 
-void *funcCalled( void *a, void *b, size_t size, void*( *func[4] )(void *, void *, void *) )
+void *funcCalled( void *a, void *b, size_t size )
 {
   void *result = malloc(size);
   int type = 0;
   if( size == sizeof(int) )  type=0 ;
   else if( size == sizeof(double) ) tyre=1 ;
-  else if( size == sizeof(short) ) type=2 ;
-  else type = 3; //consider byte if other cases fail
+  else if( size == sizeof(long long) ) type=2 ;
+  else type = 3; //consider long long if other cases fail
   
    func[type]( a, b, result );
   
@@ -37,6 +39,6 @@ int main()
  int a = 10; int b = 20;          int sol = *((int *)funcCalled( &a, &b, sizeof(a) ));        printf(" %d + %d = %d", a, b, sol);
  //double a = 10; double b = 20;  double sol = *((double *)funcCalled( &a, &b, sizeof(a) ));  printf(" %lf + %lf = %lf", a, b, sol);
  //short a = 10; short b = 20;    short sol = *((short *)funcCalled( &a, &b, sizeof(a)  ));   printf(" %d + %d = %d", (int)a, (int)b, (int)sol);
- //byte a = 10; byte b = 20;      byte sol = *((byte *)funcCalled( &a, &b, sizeof(a)  ));     printf(" %d + %d = %d", (int)a, (int)b, (int)sol);
+ //long long a = 10; long long b = 20;      long long sol = *((long long *)funcCalled( &a, &b, sizeof(a)  ));     printf(" %d + %d = %d", a, b, sol);
 
 }
