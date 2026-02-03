41a6a3f48a30a8692822fcf9d1f351acd3dad1ec
durvesh <thes86088@gmail.com>
2026-02-03 21:30:42 +0530

    add approximate body to funcCalled

--- methods.c
+++ methods.c
@@ -4,3 +4,26 @@ void *add( void *a, void *b ){
   result = (void *)( (*((type *)a)) + (*((type *)b)) )  ;
   return result ;
 }
+
+size_t size = sizeof(a)>sizeof(b) ? sizeof(a) : sizeof(b) ;
+
+
+
+void *funcCalled( void *a, void *b, size_t size, void*( *func[4] )(void *, void *, void *) )
+{
+  void *result = malloc(size);
+
+  if( size == sizeof(int) )
+    func[0]( a, b, result );
+  
+  else if( size == sizeof(double) )
+    func[1]( a, b, result );
+  
+  else if( size == sizeof(short) )
+    func[2]( a, b, result );
+
+  else if( size == sizeof(byte) )
+    func[3]( a, b, result );
+  
+  return result ;
+}
