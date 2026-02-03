ec7e185095e07790028a58039f6259f2a5a081f3
durvesh <thes86088@gmail.com>
2026-02-03 20:44:59 +0530

    add body for main function

--- methods.c
+++ methods.c
@@ -4,3 +4,12 @@ int *mul( int(*a), int(*b) ){ return (*a)*(*b); }
 int *divi( int(*a), int(*b) ){ return (*a)/(*b); }
 
 int *((*func)[4])(int *, int *) = {add, sub, mul, divi};
+
+int main()
+{
+  int a = 10; int b=20;
+  printf(" %d + %d = %d \n", a, b, *(func[0](a,b)) );
+  printf(" %d - %d = %d \n", a, b, *(func[1](a,b)) );
+  printf(" %d * %d = %d \n", a, b, *(func[2](a,b)) );
+  printf(" %d / %d = %d \n", a, b, *(func[3](a,b)) );
+}
